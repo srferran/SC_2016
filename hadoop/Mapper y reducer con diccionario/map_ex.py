4f850ed981353042d10032ecea5e3a6a2922746a
srferran <sr.ferran@gmail.com>
2016-06-01 09:21:01 +0200

    final

--- hadoop/Mapper y reducer con diccionario/map_ex.py
+++ hadoop/Mapper y reducer con diccionario/map_ex.py
@@ -8,24 +8,30 @@ for line in sys.stdin:
 	# split the line into words
 	words = line.strip().split(',')
 	
+	# read year, airport, delay and cancelled status
 	year = words[0]
 	airport = words[16]
 	delay = words[15]
 	cancelled = words[21] == "1"
 	
 	
-	
+	# Only for not cancelled flights
+	# cast delay from string to float
 	if not cancelled:
 		try:
 			delay = float(delay)
 		except ValueError:
 			continue
-		
+		# check if key exists at dict
+		# If not exist make new key & init to [current delay value, number of processed items = 1 ]
 		if (year, airport) not in d:
 			d[(year, airport)] = [delay, 1]
+		#if key exists add delay and increase processed items by 1
 		else:
 			d[(year, airport)][0] += delay
 			d[(year, airport)][1] += 1
+
+# Output from mapper  
 # flush dict
 for y, a in d:
 	print "%s,%s\t%f\t%d" % (y, a, d[(y, a)][0], d[(y, a)][1])
--- hadoop/Mapper y reducer con diccionario/red_ex.py
+++ hadoop/Mapper y reducer con diccionario/red_ex.py
@@ -6,12 +6,17 @@ import sys
 current_airport = None
 current_counts = 0
 current_delay = 0
+current_year = 0
+airport = ""
 # input comes from STDIN
 for line in sys.stdin:
 	# remove leading and trailing whitespace
 	#line = line.strip()
 	# parse the input we got from mapper.py
+	# split into key =(year, airport) delay = delay, counts = processed items for this (year,airport) pair
+
 	key, delay, counts = line.split('\t')
+	# split key into year, airport
 	year, airport = key.split(',')
 	
 	try:
@@ -19,21 +24,27 @@ for line in sys.stdin:
 		counts = int(counts)
 	except ValueError:
 		continue
-	
+
+	# while input has same airport add values
 	if current_airport == airport:
 		current_delay += delay
 		current_counts += counts
-
+		
+		# if new airport it's loaded just output current stored data
 	else:
+		# avoid initial case or no items processed. ( current_airport = None)
 		if current_airport:
+			
 			# write result to STDOUT
 			mean = current_delay/current_counts
-			print '%s\t%s\t%f' % (year,current_airport, mean)  
+			print '%s\t%s\t%f' % (current_year,current_airport, mean)  
 
 		current_delay = delay
 		current_counts = counts
 		current_airport = airport
-
+		current_year = year
+		
+# last iteration
 if current_airport == airport:
 	mean = current_delay/current_counts
-	print '%s\t%s\t%f' % (year,current_airport, mean)
+	print '%s\t%s\t%f' % (current_year,current_airport, mean)
